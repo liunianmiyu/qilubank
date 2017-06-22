ea069e041e81a8de2567570f4a43d936600bd979
linfengjun2 <fengjun.lin@foxmail.com>
2017-06-22 19:20:59 +0800

    optimise login page

--- src/cn/yicha/adchannel/JettyStart.java
+++ src/cn/yicha/adchannel/JettyStart.java
@@ -5,6 +5,7 @@ import com.jfinal.core.JFinal;
 public class JettyStart {
 
 	public static void main(String[] args) {
-		JFinal.start();
+		//JFinal.start();
+		JFinal.start("WebRoot", 8088, "/", 5);
 	}
 }
